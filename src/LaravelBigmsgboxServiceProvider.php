d7f1a0561c4b0e81daf71568c27b265faedfe8af
owen-oj <jubifis@gmail.com>
2021-07-04 08:58:16 +0000

    renamed config file

--- src/LaravelBigmsgboxServiceProvider.php
+++ src/LaravelBigmsgboxServiceProvider.php
@@ -21,7 +21,7 @@ class LaravelBigmsgboxServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('laravel-bigmsgbox.php'),
+                __DIR__.'/../config/laravel-bigmsgbox.php' => config_path('laravel-bigmsgbox.php'),
             ], 'config');
 
             // Publishing the views.
@@ -50,7 +50,7 @@ class LaravelBigmsgboxServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-bigmsgbox');
+        $this->mergeConfigFrom(__DIR__.'/../config/laravel-bigmsgbox.php', 'laravel-bigmsgbox');
 
         // Register the main class to use with the facade
         $this->app->singleton('laravel-bigmsgbox', function () {
