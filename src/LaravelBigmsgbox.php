8267e2291204d5dc5b6a0e3b1462e393f2f6d6e1
owen-oj <jubifis@gmail.com>
2022-06-10 21:22:12 +0000

    modified client headers

--- src/LaravelBigmsgbox.php
+++ src/LaravelBigmsgbox.php
@@ -7,13 +7,20 @@ use GuzzleHttp\Client;
 class LaravelBigmsgbox
 {
     protected Client $client;
-    protected $apiSecret;
+    protected $apiVersion;
     protected $apiKey;
     protected $senderId;
     
     public function __construct()
     {
-        $this->client = new Client(['verify' => false]);
+        $this->client = new Client([
+            'verify' => false,
+            'headers' => [
+                'X-Api-Key' => $this->apiKey,
+                'X-Api-Version' => $this->apiVersion
+            ]
+        ]);
+        
         $this->apiVersion = config('laravel-bigmsgbox.apiVersion');
         $this->apiKey = config('laravel-bigmsgbox.apiKey');
         $this->senderId = config('laravel-bigmsgbox.senderId');
@@ -28,10 +35,7 @@ class LaravelBigmsgbox
         if (is_null($this->senderId)) {
             throw new \Exception("Invalid senderId or senderId is null");
         }
-        return $this->client->get("https://api-new.bigmsgbox.com/message/send?From=$this->senderId&To={$to}&Message={$message}",[
-            'X-Api-Key' => $this->apiKey,
-            'X-Api-Version'=> $this->apiVersion
-        ]);
+        return $this->client->get("https://api-new.bigmsgbox.com/message/send?From=$this->senderId&To={$to}&Message={$message}");
         
     }
 }
